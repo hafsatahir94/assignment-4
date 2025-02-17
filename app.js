5493d393636f341ca786da6fa658418cd5964dfd
hafsatahir94 <hafsafazal94@hotmail.com>
2025-02-18 02:10:36 +0500

    comp

--- app.js
+++ app.js
@@ -1,3 +1,86 @@
-let array = [[]];
-let matrix = [[0,1,0,3]]+ "<br/>"[[1,0,1,2]] + "<br/>" [[2,1,0,1]]
-document.write[[matrix.length]]
+// let array = [[]];
+
+
+// let matrix = [
+//      [0,1,2,3]
+//     ,[1,0,1,2]
+//     ,[2,1,0,1]];
+
+// for(let i=0;i<3;i++){
+//     for(let j =0;j<4;j++){
+//         document.write(matrix[i][j]+' ')
+//     }
+//     document.write('<br>')
+// }
+
+
+
+// for(let i=1;i<=10;i++){document.write(i +"<br/>")}
+
+
+// let table = +prompt("which table")
+// let length =+prompt("till when")
+// for (let i=1; i<=length;i++)
+// document.write(table + "X" + i + "=" +table*i +"<br/>") 
+
+
+
+// let fruits= ["apple", "banana", "mango", "orange",  "strawberry"] 
+
+// for(let i=0;i<fruits.length;i++){
+// document.write(fruits[i]+ "<br/>")
+// }
+// for (let j=0; j<fruits.length;j++){
+//     document.write("element at index"+ j +"is"+ fruits[j]+"<br/>")
+// }
+
+
+
+
+// for(let i=1;i<=15;i++){document.write(i +"<br/>") }
+
+
+// for(let i=10;i>=1;i--){document.write(i +"<br/>")}
+// for(let i=0;i<=20;(i+=2)){document.write(i +"<br/>")}
+// for(let i=1;i<=19;(i+=2)){document.write(i +"<br/>")}
+// for(let i=2;i<=20;(i+=2)){ document.write(i + "k" + "<br/>");}
+
+// let array = ["cake", "applepie", "patties", "cookies", "chips"];
+// let user = prompt("What will you have please?"); 
+// let flag = false; 
+
+
+// for (let i = 0; i < array.length; i++) {
+//   if (array[i] === user) {
+//     flag = true; 
+//     break;
+//   }
+// }
+
+
+// if (flag) {
+//   alert(user + " is available."); 
+// } else {
+//   alert(user + " is not available.");
+// }
+//   let num = [24, 53, 78, 91, 12]
+//   let largest = num[0];
+//   for (let i =0;i < num.length; i++){
+//   if (num[i]> largest) {
+//     largest = num[i];}
+//   }
+//   alert("the largest num is " +largest)
+  
+
+// let num  = [24, 53, 78, 91, 12] 
+// let smallest = num[0]
+// for (let i=0;i<num.length ;i++)
+// if (num[i]<smallest) {
+//     smallest=num[i]
+
+// }
+// alert("the smallest num is" + smallest)
+
+// let table = 5
+
+// for(let i=1;i<=20;i++){document.write(table+"X" + i +"=" +table*i +"<br/>")  }
